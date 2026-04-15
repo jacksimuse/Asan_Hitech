fb6c19e7191b5cfe2fafc7e0dbd59039c20d6958
jacksimuse <cjh930306@naver.com>
2026-04-15 16:51:52 +0900

    Update Form1.Designer.cs

--- c#/Class_study2/Form1.Designer.cs
+++ c#/Class_study2/Form1.Designer.cs
@@ -30,13 +30,24 @@ namespace Class_Study2
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnDeposit = new System.Windows.Forms.Button();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.tbAccount = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAccount = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(433, 98);
+            this.button1.Location = new System.Drawing.Point(54, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(379, 159);
+            this.button1.Size = new System.Drawing.Size(274, 127);
             this.button1.TabIndex = 0;
             this.button1.Text = "속성";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,23 +55,121 @@ namespace Class_Study2
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(433, 352);
+            this.button2.Location = new System.Drawing.Point(54, 257);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(379, 159);
+            this.button2.Size = new System.Drawing.Size(274, 127);
             this.button2.TabIndex = 1;
             this.button2.Text = "월급";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // btnDeposit
+            // 
+            this.btnDeposit.Location = new System.Drawing.Point(538, 184);
+            this.btnDeposit.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDeposit.Name = "btnDeposit";
+            this.btnDeposit.Size = new System.Drawing.Size(274, 127);
+            this.btnDeposit.TabIndex = 2;
+            this.btnDeposit.Text = "입금";
+            this.btnDeposit.UseVisualStyleBackColor = true;
+            this.btnDeposit.Click += new System.EventHandler(this.btnDeposit_Click);
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Location = new System.Drawing.Point(538, 339);
+            this.btnWithdraw.Margin = new System.Windows.Forms.Padding(2);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(274, 123);
+            this.btnWithdraw.TabIndex = 3;
+            this.btnWithdraw.Text = "출금";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            this.btnWithdraw.Click += new System.EventHandler(this.btnWithdraw_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(77, 116);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(375, 35);
+            this.tbName.TabIndex = 4;
+            // 
+            // tbAccount
+            // 
+            this.tbAccount.Location = new System.Drawing.Point(77, 305);
+            this.tbAccount.Name = "tbAccount";
+            this.tbAccount.Size = new System.Drawing.Size(375, 35);
+            this.tbAccount.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCheck);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnAccount);
+            this.groupBox1.Controls.Add(this.tbAccount);
+            this.groupBox1.Controls.Add(this.tbName);
+            this.groupBox1.Controls.Add(this.btnWithdraw);
+            this.groupBox1.Controls.Add(this.btnDeposit);
+            this.groupBox1.Location = new System.Drawing.Point(401, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(829, 678);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "계좌번호";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(73, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "이름";
+            // 
+            // btnAccount
+            // 
+            this.btnAccount.Location = new System.Drawing.Point(538, 33);
+            this.btnAccount.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAccount.Name = "btnAccount";
+            this.btnAccount.Size = new System.Drawing.Size(274, 127);
+            this.btnAccount.TabIndex = 6;
+            this.btnAccount.Text = "계좌생성";
+            this.btnAccount.UseVisualStyleBackColor = true;
+            this.btnAccount.Click += new System.EventHandler(this.btnAccount_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(77, 394);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(274, 127);
+            this.btnCheck.TabIndex = 9;
+            this.btnCheck.Text = "계좌확인";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(18F, 30F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1222, 613);
+            this.ClientSize = new System.Drawing.Size(1233, 721);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +178,15 @@ namespace Class_Study2
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnDeposit;
+        private System.Windows.Forms.Button btnWithdraw;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.TextBox tbAccount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnAccount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnCheck;
     }
 }
 
